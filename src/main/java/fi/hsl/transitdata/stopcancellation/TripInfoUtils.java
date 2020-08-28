1825b9dae22a7e9370b3b5b6fdd8015c7debcbdf
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2020-08-28 11:18:59 +0300

    Send trip updates for earlier trips earlier

--- src/main/java/fi/hsl/transitdata/stopcancellation/TripInfoUtils.java
+++ src/main/java/fi/hsl/transitdata/stopcancellation/TripInfoUtils.java
@@ -1,9 +1,10 @@
 package fi.hsl.transitdata.stopcancellation;
 
 import com.google.transit.realtime.GtfsRealtime;
-import fi.hsl.common.transitdata.PubtransFactory;
-import fi.hsl.common.transitdata.RouteIdUtils;
-import fi.hsl.common.transitdata.proto.InternalMessages;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TripInfoUtils {
     private TripInfoUtils() {}
@@ -11,5 +12,16 @@ public class TripInfoUtils {
     //TODO: this should be in common as rail-tripupdate-source
     public static String getTrainEntityId(GtfsRealtime.TripDescriptor tripDescriptor) {
         return "rail_" + String.join("-", tripDescriptor.getRouteId(), tripDescriptor.getStartDate(), tripDescriptor.getStartTime(), String.valueOf(tripDescriptor.getDirectionId()));
+    }
+
+    public static LocalDateTime getStartTime(GtfsRealtime.TripDescriptor tripDescriptor) {
+        final LocalDate date = LocalDate.parse(tripDescriptor.getStartDate(), DateTimeFormatter.BASIC_ISO_DATE);
+
+        final String[] time = tripDescriptor.getStartTime().split(":");
+        final int hours = Integer.parseInt(time[0]);
+        final int minutes = Integer.parseInt(time[1]);
+        final int seconds = Integer.parseInt(time[2]);
+
+        return date.plusDays(hours / 24).atTime(hours % 24, minutes, seconds);
     }
 }
